45048e425b45f8e1999c4493ddfecce67a9d85da
Richard <ricky201325@gmail.com>
2025-01-28 00:33:51 -0300

    registrar usuario

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,27 @@
+import bcrypt from "bcrypt";
+import { pool } from "../db.js";
+
 export const signin = (req, res) => res.send("Ingresando");
-export const signup = (req, res) => res.send("registrado");
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword)
+    const result = await pool.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, hashedPassword]
+    );
+    console.log(result);
+    return res.json(result.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.status(400).json({
+        message: "El correo ya esta registrado",
+      });
+    }
+  }
+};
 
 export const signout = (req, res) => res.send("cerrando sesion");
 
