4c3a04567f10fbe3ab52af740ee73c1b37b1985b
Richard <ricky201325@gmail.com>
2025-01-28 15:32:10 -0300

    termiando el setup de backend

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,8 +1,9 @@
 import { pool } from "../db.js";
 
 export const getAllTasks = async (req, res, next) => {
-  console.log(req.userId);
-  const result = await pool.query("SELECT * FROM task");
+  const result = await pool.query("SELECT * FROM task WHERE user_id = $1", [
+    req.userId,
+  ]);
   return res.json(result.rows);
 };
 export const getTask = async (req, res) => {
@@ -19,8 +20,8 @@ export const createTask = async (req, res, next) => {
   const { title, description } = req.body;
   try {
     const result = await pool.query(
-      "INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *",
-      [title, description]
+      "INSERT INTO task (title, description, user_id) VALUES ($1, $2, $3) RETURNING *",
+      [title, description, req.userId]
     );
     res.json(result.rows[0]);
   } catch (error) {
