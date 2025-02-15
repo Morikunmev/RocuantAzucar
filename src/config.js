5c0d47f30f5231e6b18beb3be6441fcfcc4b98e9
Richard <ricky201325@gmail.com>
2025-02-15 20:58:55 -0300

    Cambiado el config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 3000;
-export const PG_PORT = process.env.PORT || 5432;
+export const PG_PORT = process.env.PG_PORT || 5432;  // Cambiado
 export const PG_HOST = process.env.PG_HOST || "localhost";
 export const PG_USER = process.env.PG_USER || "postgres";
 export const PG_PASSWORD = process.env.PG_PASSWORD || "xlgricky20131415";
