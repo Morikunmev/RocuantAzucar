ebfedac868f02de70c458280f1eb4ea09a38c146
Richard <ricky201325@gmail.com>
2025-02-16 02:34:07 -0300

    vite.config.js app.js, config.js cambiado url del front

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://zooming-wholeness-production.up.railway.app",
+      "https://rocuantazucarfront.up.railway.app/login",
     ],
     credentials: true,
   })
